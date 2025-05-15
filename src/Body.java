27065bfa800460d15ba9c6b04ea0957943335c1f
Yousra0225 <chbibyousra02@gmail.com>
2025-05-15 22:10:58 +0200

    fix(body) : fix the issues

--- src/Body.java
+++ src/Body.java
@@ -1,30 +1,33 @@
-class Body {
+public class Body {
     public String component;
 
     public Body(int length, int doors) {
-        String firstLine = " " + "_".repeat(length - 2) + "\n";
+        StringBuilder sb = new StringBuilder();
+        sb.append(" ");
+        for (int i = 0; i < length - 2; i++) sb.append("_");
+        sb.append("\n|");
 
-        char[] secondLine = new char[length - 1];
-        secondLine[0] = '|';
-        for (int i = 1; i < length - 2; i++) secondLine[i] = ' ';
-        secondLine[length - 2] = '\\';
-        secondLine[length - 1 - 1] = '\n';
-        int[] doorPositions = new int[(length - 3)];
-        int front = 0, rear = doorPositions.length - 1;
-        boolean addFront = true;
+        char[] line = new char[length - 2];
+        for (int i = 0; i < line.length; i++) line[i] = ' ';
+        line[line.length - 1] = '\\';
 
+        // Placer les portes
+        int[] positions = new int[doors];
+        int left = 0, right = (length - 4) / 2;
+        boolean leftTurn = true;
         for (int i = 0; i < doors; i++) {
-            int pos = addFront ? front++ : rear--;
-            doorPositions[i] = pos;
-            addFront = !addFront;
+            positions[i] = leftTurn ? left++ : right--;
+            leftTurn = !leftTurn;
+        }
+        for (int i = 0; i < doors; i++) {
+            int pos = 1 + positions[i] * 2;
+            if (pos + 1 < line.length) {
+                line[pos] = '[';
+                line[pos + 1] = ']';
+            }
         }
 
-        for (int i = 0; i < doors; i++) {
-            int pos = 1 + doorPositions[i];
-            secondLine[pos] = '[';
-            secondLine[pos + 1] = ']';
-        }
-
-        this.component = firstLine + new String(secondLine);
+        sb.append(new String(line)).append("\n");
+        this.component = sb.toString();
     }
 }
