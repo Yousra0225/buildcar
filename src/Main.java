85b5bcaa883102a9fa2a38b18ef2d6fa06ba0945
Yousra0225 <chbibyousra02@gmail.com>
2025-05-15 22:13:27 +0200

    feat(main): add main class to test the build of a car

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
-public class main {
-    public static void main(String[] args){
-        Car car = new Car();
-        System.out.println(car);
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Car smallCar = new Car(7, 1);
+            Car mediumCar = new Car(12, 2);
+            Car largeCar = new Car(17, 5);
+
+            System.out.println("Small Car:");
+            smallCar.draw();
+            System.out.println("\nMedium Car:");
+            mediumCar.draw();
+            System.out.println("\nLarge Car:");
+            largeCar.draw();
+
+        } catch (Exception e) {
+            System.out.println("Erreur : " + e.getMessage());
+        }
     }
 }
