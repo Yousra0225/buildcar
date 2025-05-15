1a826d2646437dd8a0a3a9451913fbea0bbf612e
Yousra0225 <chbibyousra02@gmail.com>
2025-05-15 22:12:36 +0200

    update

--- src/Chassis.java
+++ src/Chassis.java
@@ -1,47 +1,21 @@
-class Chassis {
+public class Chassis {
     public String component;
+
     public Chassis(int length) {
-        StringBuilder chassisLine = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+        sb.append("-");
 
-        chassisLine.append("-");
-        int axleCount = 2;
-        int extra = length - 7;
-
-        if (length >= 12) {
-            axleCount += (length - 12) / 2 + 1;
-        }
-        boolean rear = true;
-        int left = 0, right = 0;
-        int[] positions = new int[axleCount];
-        positions[0] = 1; positions[1] = 3;
-
-        int pos = 5;
-        for (int i = 2; i < axleCount; i++) {
-            positions[i] = pos;
-            pos += 2;
-        }
-
-        for (int i = 0; i < length; i++) {
-            if (contains(positions, i)) {
-                chassisLine.append("o");
-            } else if (i == length - 1) {
-                chassisLine.append("'");
+        int wheels = (length - 1) / 4 + 1;
+        for (int i = 1; i < length - 1; i++) {
+            if (i % 2 == 1 && wheels > 0) {
+                sb.append("o");
+                wheels--;
             } else {
-                chassisLine.append("-");
+                sb.append("-");
             }
         }
+        sb.append("'");
 
-        this.component = chassisLine.toString();
-    }
-
-    /**
-     * method to check if an array contains a value
-     * @param arr the array to check
-     * @param value the value to check for
-     * @return true if the array contains the value, false otherwise
-     */
-    private boolean contains(int[] arr, int value) {
-        for (int a : arr) if (a == value) return true;
-        return false;
+        this.component = sb.toString();
     }
 }
