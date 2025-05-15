08c8ca146d6bea3151cd5bf684cca5705c38acc6
Yousra0225 <chbibyousra02@gmail.com>
2025-05-15 21:55:34 +0200

    feat(car): add contructor to car class

--- src/Car.java
+++ src/Car.java
@@ -1,14 +1,15 @@
-public class Car {
-    public String chassis = "__";
-    public String roue = "O";
-    public String body =
-            "  ____   " +
-           "__/ [] \__" +
-          "|__ ____ __|" +
-          "   0    0   "  ;
+class Car {
+    public Body body;
+    public Chassis chassis;
 
-    public Car(){
-        this.body = body;
+    public Car(int length, int doors) throws Exception {
+        if (length < 7) throw new Exception("Car too short");
+        if (doors < 1) throw new Exception("At least 1 door required");
+
+        int maxDoors = (length - 3) / 2; // positions utilisables
+        if (doors > maxDoors) throw new Exception("Too many doors");
+
+        this.body = new Body(length, doors);
+        this.chassis = new Chassis(length);
     }
 }
-
