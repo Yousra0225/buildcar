e40d2f423e8a4d899561b704c86383111e147f52
Yousra0225 <chbibyousra02@gmail.com>
2025-05-15 21:57:52 +0200

    feat(chassis): add contructor of chassis class

--- src/Chassis.java
+++ src/Chassis.java
@@ -1,3 +1,38 @@
-public class Chassis {
-    public Ca
+class Chassis {
+    public String component;
+    public Chassis(int length) {
+        StringBuilder chassisLine = new StringBuilder();
+
+        chassisLine.append("-");
+        int axleCount = 2;
+        int extra = length - 7;
+
+        if (length >= 12) {
+            axleCount += (length - 12) / 2 + 1;
+        }
+        boolean rear = true;
+        int left = 0, right = 0;
+        int[] positions = new int[axleCount];
+        positions[0] = 1; positions[1] = 3;
+
+        int pos = 5;
+        for (int i = 2; i < axleCount; i++) {
+            positions[i] = pos;
+            pos += 2;
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (contains(positions, i)) {
+                chassisLine.append("o");
+            } else if (i == length - 1) {
+                chassisLine.append("'");
+            } else {
+                chassisLine.append("-");
+            }
+        }
+
+        this.component = chassisLine.toString();
+    }
+
+   
 }
